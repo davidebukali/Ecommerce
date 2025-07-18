8a778bf8273055922710c1e1d8ad202c226fd254
David <debukali@gmail.com>
2025-07-18 19:54:47 +0300

    Add relationship between cart and cartitem

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,7 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\CartItems;
+
 class Cart extends Model
 {
-    //
+    protected $fillable = ['user_id', 'session_id', 'status'];
+
+    public function CartItems()
+    {
+        return $this->hasMany(CartItems::class);
+    }
 }
--- app/Models/CartItems.php
+++ app/Models/CartItems.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cart;
 
 class CartItems extends Model
 {
-    //
+    protected $table = 'cart_items';
+    protected $fillable = ['cart_id', 'product_id', 'quantity', 'price', 'total'];
+
+    public function Cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
 }
