64d2e240b62c8848cae0d4f307de66642801cd02
David <debukali@gmail.com>
2025-07-19 16:54:03 +0300

    Add a product relationship to the cart

--- app/Models/CartItems.php
+++ app/Models/CartItems.php
@@ -10,8 +10,13 @@ class CartItems extends Model
     protected $table = 'cart_items';
     protected $fillable = ['cart_id', 'product_id', 'quantity', 'price', 'total'];
 
-    public function Cart()
+    public function cart()
     {
         return $this->belongsTo(Cart::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
     }
 }
