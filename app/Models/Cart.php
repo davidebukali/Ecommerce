3f476afccfd66f4648b694336c2faada730277aa
David <debukali@gmail.com>
2025-07-19 16:54:25 +0300

    Rename cart items relationship to items

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -10,7 +10,7 @@ class Cart extends Model
 {
     protected $fillable = ['user_id', 'session_id', 'status'];
 
-    public function CartItems()
+    public function items()
     {
         return $this->hasMany(CartItems::class);
     }
