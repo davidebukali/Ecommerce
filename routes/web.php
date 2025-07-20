f41137aea74b2adfb93538ae2cbaddaf0f0f0cd4
David <debukali@gmail.com>
2025-07-20 15:06:42 +0300

    Add checkout route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,5 @@ Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.a
 Route::post('/cart/update', [CartController::class, 'updateCart'])->name('cart.update');
 Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
 
-Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
-Route::post('/checkout', [CheckoutController::class, 'process'])->name('checkout.process');
+Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout.process');
 
