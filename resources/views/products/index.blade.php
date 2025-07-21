7039626f661eae5f50f80e091adbe00a311099d5
David <debukali@gmail.com>
2025-07-21 10:44:05 +0300

    Add html when the product listing is empty

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,8 +3,15 @@
 @section('content')
 <h1>Products</h1>
 <div class="product-list">
+    @if ($products->isEmpty())
+    <div class="empty-state">
+        <p>No products available yet.</p>
+        <a href="{{ route('products.index') }}" class="button-primary">Refresh</a>
+    </div>
+    @else
     @foreach($products as $product)
     @include('components.product-card', ['product' => $product, 'mainImage' => $product->mainImage])
     @endforeach
+    @endif
 </div>
 @endsection
