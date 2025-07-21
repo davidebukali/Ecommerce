20c0167237280234a56a4a1871cf5b6290f6d33f
David <debukali@gmail.com>
2025-07-21 09:12:58 +0300

    Add HasApiTokens to User model for creating tokens

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    // if (! $user || ! Hash::check($request->password, $user->password)) {
+    //     throw ValidationException::withMessages([
+    //         'email' => ['The provided credentials are incorrect.'],
+    //     ]);
+    // }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
