98a4de799d2be1e62cf5be69b4ddc9ef7711c0aa
David <debukali@gmail.com>
2025-07-21 11:55:16 +0300

    Add factories to cart and cartitem models

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,11 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use App\Models\CartItems;
 
 class Cart extends Model
 {
+    use HasFactory;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['user_id', 'session_id', 'status'];
 
     public function items()
--- app/Models/CartItems.php
+++ app/Models/CartItems.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Cart;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CartItems extends Model
 {
+    use HasFactory;
+    
     protected $table = 'cart_items';
     protected $fillable = ['cart_id', 'product_id', 'quantity', 'price', 'total'];
 
