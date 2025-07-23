8a6ecf912bd6b515f5cc8b07112d5be12088002f
David <debukali@gmail.com>
2025-07-23 11:20:24 +0300

    Listen for websocket messages on a private channel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+Pusher.logToConsole = true;
+document.addEventListener("DOMContentLoaded", function () {
+    const orderInput = document.getElementById("orderId");
+
+    if (orderInput) {
+        const orderId = orderInput.value;
+        Echo.private("orders." + orderId)
+        .listen("OrderStatusUpdated", (e) => { // Use event class name, a custom event name works only with pusher
+            alert("Order status updated: " + JSON.stringify(e));
+        });
+    } else {
+        console.error("Order ID not found in the DOM.");
+    }
+    
+});
