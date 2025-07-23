55a94ce49bc19be3bd518a43efb4652a435a21b7
David <debukali@gmail.com>
2025-07-23 11:18:52 +0300

    Create an order staus updated event

---
+++ app/Events/OrderStatusUpdated.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Order;
+
+class OrderStatusUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $order;
+    public $oldStatus;
+    public $newStatus;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Order $order, string $oldStatus, string $newStatus)
+    {
+        $this->order = $order;
+        $this->oldStatus = $oldStatus;
+        $this->newStatus = $newStatus;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('orders.'.$this->order->id),
+        ];
+    }
+}
